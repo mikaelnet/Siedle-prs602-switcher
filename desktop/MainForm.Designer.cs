bb877d408ffc9f2ef2fe7809555896e03ae10164
Mikael Högberg <mikael.hogberg@stendahls.se>
2021-02-14 17:45:52 +0100

    desktop application

---
+++ desktop/MainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SiedleInputSwitcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comPortDropdown = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonA = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.consoleTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select COM port";
+            // 
+            // comPortDropdown
+            // 
+            this.comPortDropdown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comPortDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPortDropdown.FormattingEnabled = true;
+            this.comPortDropdown.Location = new System.Drawing.Point(13, 30);
+            this.comPortDropdown.Name = "comPortDropdown";
+            this.comPortDropdown.Size = new System.Drawing.Size(155, 21);
+            this.comPortDropdown.TabIndex = 1;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonA
+            // 
+            this.buttonA.Location = new System.Drawing.Point(12, 76);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(75, 75);
+            this.buttonA.TabIndex = 2;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = true;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(174, 29);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(174, 346);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.disconnectButton.Location = new System.Drawing.Point(174, 58);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectButton.TabIndex = 5;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(93, 76);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(75, 75);
+            this.buttonB.TabIndex = 6;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = true;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // buttonC
+            // 
+            this.buttonC.Location = new System.Drawing.Point(174, 76);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(75, 75);
+            this.buttonC.TabIndex = 7;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
+            // 
+            // offButton
+            // 
+            this.offButton.Location = new System.Drawing.Point(12, 157);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(237, 23);
+            this.offButton.TabIndex = 8;
+            this.offButton.Text = "off";
+            this.offButton.UseVisualStyleBackColor = true;
+            this.offButton.Click += new System.EventHandler(this.offButton_Click);
+            // 
+            // consoleTextBox
+            // 
+            this.consoleTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.consoleTextBox.Location = new System.Drawing.Point(13, 203);
+            this.consoleTextBox.Multiline = true;
+            this.consoleTextBox.Name = "consoleTextBox";
+            this.consoleTextBox.ReadOnly = true;
+            this.consoleTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.consoleTextBox.Size = new System.Drawing.Size(236, 137);
+            this.consoleTextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Serial console";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 381);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.consoleTextBox);
+            this.Controls.Add(this.offButton);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.comPortDropdown);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Siedle input switcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comPortDropdown;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button offButton;
+        private System.Windows.Forms.TextBox consoleTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ desktop/MainForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiedleInputSwitcher
+{
+    public partial class MainForm : Form
+    {
+        private readonly Color _defaultColor;
+        private delegate void DataReceivedDelegate(string str);
+        private readonly DataReceivedDelegate _dataReceived;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadComPorts();
+            MoveDisconnectButton();
+            UpdateControls();
+            _defaultColor = buttonA.BackColor;
+            _dataReceived = ReceivedData;
+        }
+
+        public void LoadComPorts()
+        {
+            comPortDropdown.Items.Clear();
+
+            var configuredPort = Properties.Settings.Default["ComPort"];
+            foreach (var comPortName in SerialPort.GetPortNames())
+            {
+                comPortDropdown.Items.Add(comPortName);
+                if (string.Equals(comPortName, configuredPort))
+                {
+                    comPortDropdown.SelectedItem = comPortName;
+                }
+            }
+        }
+
+        private void MoveDisconnectButton()
+        {
+            disconnectButton.Left = connectButton.Left;
+            disconnectButton.Top = connectButton.Top;
+            disconnectButton.Width = connectButton.Width;
+            disconnectButton.Height = connectButton.Height;
+            disconnectButton.Visible = false;
+        }
+
+        private void UpdateControls()
+        {
+            var connected = serialPort1.IsOpen;
+
+            buttonA.Enabled = connected;
+            buttonB.Enabled = connected;
+            buttonC.Enabled = connected;
+            offButton.Enabled = connected;
+
+            comPortDropdown.Enabled = !connected;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            disconnectButton_Click(sender, e);
+            Application.Exit();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            var port = comPortDropdown.SelectedItem as string;
+            if (string.IsNullOrWhiteSpace(port))
+                return;
+
+            serialPort1.PortName = port;
+            serialPort1.Open();
+
+            disconnectButton.Visible = true;
+            connectButton.Visible = false;
+
+            SetColor(-1);
+
+            UpdateControls();
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            disconnectButton.Visible = false;
+            connectButton.Visible = true;
+
+            if (!serialPort1.IsOpen)
+                return;
+            
+            SendCommand("0");
+
+            serialPort1.Close();
+            Properties.Settings.Default["ComPort"] = serialPort1.PortName;
+            Properties.Settings.Default.Save();
+
+            UpdateControls();
+        }
+
+        private void SendCommand(string command)
+        {
+            if (!serialPort1.IsOpen)
+                return;
+
+            serialPort1.WriteLine(command);
+        }
+
+        private void SetRelay(int relay)
+        {
+            if (relay < 0 || relay > 3)
+                return;
+
+            char cmd = (char) ('0' + relay);
+            SendCommand($"{cmd}");
+        }
+
+        private void SetColor(int relay)
+        {
+            var buttons = new[] {offButton, buttonA, buttonB, buttonC};
+            foreach (var btn in buttons)
+            {
+                btn.BackColor = _defaultColor;
+            }
+
+            if (relay < 0 || relay > 3)
+                return;
+
+            buttons[relay].BackColor = relay == 0 ? Color.LightCoral : Color.LightGreen;
+        }
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            SetRelay(1);
+        }
+
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            SetRelay(2);
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            SetRelay(3);
+        }
+
+        private void offButton_Click(object sender, EventArgs e)
+        {
+            SetRelay(0);
+        }
+
+        private void ReceivedData(string str)
+        {
+            consoleTextBox.AppendText(str);
+            bool isComment = false;
+            foreach (char ch in str)
+            {
+                if (ch == '\r' || ch == '\n')
+                {
+                    isComment = false;
+                    continue;
+                }
+
+                if (ch == '#')
+                {
+                    isComment = true;
+                    continue;
+                }
+
+                // Ignore all characters in a comment block
+                if (isComment)
+                    continue;
+
+                if (ch >= '0' && ch <= '3')
+                {
+                    int relay = ch - '0';
+                    SetColor(relay);
+                }
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var serialPort = (SerialPort) sender;
+            string data = serialPort.ReadExisting();
+            Invoke(_dataReceived, data);
+        }
+    }
+}
